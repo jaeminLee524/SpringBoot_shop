34f81c1dea1cd3a728489bde446967f919f9bf9a
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-03 14:26:21 +0900

    회원 도메인 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -11,13 +12,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberRepository {
 
+    @Autowired
     private final EntityManager em;
 
     // 저장
-    public Member save(Member member) {
+    public void save(Member member) {
         em.persist(member);
-
-        return member;
     }
 
     // 한명의 멤버 조회
@@ -26,6 +26,7 @@ public class MemberRepository {
         return em.find(Member.class, id);
     }
 
+    //TODO Optional로 변경예정
     // 멤버 리스트 조회
     public List<Member> findAll() {
         List<Member> result = em.createQuery("select m from Member m", Member.class)
@@ -34,6 +35,7 @@ public class MemberRepository {
         return result;
     }
 
+    //TODO Optional로 변경예정
     // 이름으로 조회
     public List<Member> findByName(String name) {
         List<Member> result = em.createQuery("select m from Member m where name = :name", Member.class)
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -24,6 +24,8 @@ public class MemberService {
         // 중복 회원 체크
         validateDuplicateMember(member);
 
+        memberRepository.save(member);
+
         // em.persist가 되는 순간, persistenceContext에 member객체를 올림,
         // key는 db와 매핑한 id값이 되고, 값은 @GeneratedValue로 값이 생심 => persist하는 당시에 항상 값이 세팅되어있음
         // member.getId는 항상 값이 있다는게 보장됨
@@ -33,6 +35,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if(!findMembers.isEmpty() ) {
+            System.out.println("==============================================테스트==============================================");
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
@@ -47,4 +50,7 @@ public class MemberService {
     /**
      * 회원 한명 조회
      */
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
 }
