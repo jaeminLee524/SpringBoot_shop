8ea656cc401e86b5cee7b65abff177bb8c273666
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-03 14:57:52 +0900

    Item Entity 객체지향에 맞는 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==//
+    // 데이터를 갖고 있는 곳에서 비즈니스 메서드가 있는게 응집력에 좋다.
+    /**
+     * stock(재고) 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock(재고) 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if( restStock < 0 ) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity =restStock;
+    }
 }
