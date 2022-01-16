2a925c8cc23fc210dc91007d76fec85f2bbb8e4a
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-17 02:16:15 +0900

    findAll메서드 추가

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -42,6 +42,11 @@ public class OrderRepository {
         return resultList;
     }
 
+    public List<Order> findAll() {
+        return em.createQuery("select o from Order o", Order.class)
+                .getResultList();
+    }
+
     public List<Order> findAllByString(OrderSearch orderSearch) {
         String jpql = "select o From Order o join o.member m";
         boolean isFirstCondition = true;
@@ -98,5 +103,12 @@ public class OrderRepository {
         cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
         return query.getResultList();
+    }
+
+    // 패치 조인을 이용
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery("select o from Order o " +
+                " join fetch o.member m" +
+                " join fetch m.delivery d", Order.class).getResultList();
     }
 }
