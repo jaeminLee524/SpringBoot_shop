8962f0b712eea24e077dc9391e61634ba0f2f57e
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-17 02:15:37 +0900

    유연성 향상을 위해 객체 래퍼

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,10 +22,33 @@ public class MemberApiController {
     private final MemberService memberService;
 
     /**
+     * 회원조회
+     */
+    @GetMapping("/members")
+    public Result findMembers() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName())).collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+    /**
      * 회원가입
      * request로 별도 DTO를 받는다
      */
-    @PostMapping("/v2/members")
+    @PostMapping("/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
 
         Member member = new Member();
@@ -33,7 +58,7 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
-    @PutMapping("/v2/members/{id}")
+    @PutMapping("/members/{id}")
     public UpdateMemberResponse updateMemberV2(@PathVariable Long id, @RequestBody @Valid UpdateMemberRequest request) {
         log.info("id, request : {}", request, id);
         memberService.update(id, request.getName());
