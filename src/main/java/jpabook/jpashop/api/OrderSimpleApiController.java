36ff7e0c1f1b40fea42b2339894bc9b7c84b8967
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-12 12:38:42 +0900

    version4 로직 추가 => dto직접 조회 로직

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,9 +29,10 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/v1/simple-orders") //v1 : entity직접 노출
-    public List<Order> orderVersion1() {
+    public List<Order> ordersV1() {
         //1. entity를 노출하게 되면 양방향 관계가 걸린 곳에서 무한 루프가 걸림 => @JsonIgnore로 해결
         //2. xToOne관계에서 LAZY로 발라논 객체들을 json이 해결을 하지 못함 entity를 갖고오는게 아닌 proxy객체를 참조하기에
         //      => hibernate5Module의 ForceLazyLoading으로 해결
@@ -46,7 +49,7 @@ public class OrderSimpleApiController {
 
     //v2의 문제점: LAZY로딩으로 인한 query가 많이 발생
     @GetMapping("/v2/simple-orders")
-    public List<SimpleOrderDto> orderVersion2() { //v2: dto로 
+    public List<SimpleOrderDto> ordersV2() { //v2: dto로
         List<Order> orders = orderRepository.findAll();
         List<SimpleOrderDto> result = orders.stream().map(o -> new SimpleOrderDto(o))
                 .collect(Collectors.toList());
@@ -56,7 +59,7 @@ public class OrderSimpleApiController {
 
     // entity로 조회 -> dto로 변환
     @GetMapping("/v3/simple-orders")
-    public List<SimpleOrderDto> orderVersion3() {
+    public List<SimpleOrderDto> ordersV3() {
         //ex) 조회결과 데이터 : 2개
         // => map을 수행하면서 N + 1 문제 발생 => 1 + 회원 N + 배송 N
         List<Order> orders = orderRepository.findAllWithMemberDelivery();
@@ -66,6 +69,15 @@ public class OrderSimpleApiController {
 
         return result;
     }
+
+    // v3보다는 원하는 결과 fit하게 갖고올 수 있다는 장점으로 성능 최적화가 가능(생각보다 미비함)
+    // 하지만 dto에 맞게 구현된 로직이라 재활용할 수가 없다 => api 스펙에 맞춘 코드
+    // 조회하는 field의 개수가 많고 실시간 트래픽이 많다면 api에 스펙에 맞춰 해당 로직을 구현하는걸 고려하는게 맞음
+    @GetMapping("v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
 
     @Data
     static class SimpleOrderDto {
