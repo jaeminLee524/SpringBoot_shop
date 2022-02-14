aa769d23cdd6bddf760d69e57bb47c9497c8b501
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-15 02:39:25 +0900

    dto직접 조회 repo

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,7 +1,7 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -12,34 +12,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
 
-    /**
-     * 주문 저장
-     */
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+    }
+
     public void save(Order order) {
         em.persist(order);
     }
 
-    /**
-     *  단건 주문 조회
-     */
     public Order findOne(Long id) {
         return em.find(Order.class, id);
-    }
-
-    /**
-     * 조회
-     */
-    public List<Order> findAll(OrderSearch orderSearch){
-        List<Order> resultList = em.createQuery("select o from Order o join o.member m" +
-                        "where o.status= :status" +
-                        "and m.name like :name", Order.class)
-                .getResultList();
-        return resultList;
     }
 
     public List<Order> findAll() {
@@ -48,26 +34,28 @@ public class OrderRepository {
     }
 
     public List<Order> findAllByString(OrderSearch orderSearch) {
+        //language=JPAQL
         String jpql = "select o From Order o join o.member m";
         boolean isFirstCondition = true;
-
         //주문 상태 검색
-        if(orderSearch.getOrderStatus() != null) {
-            jpql += " where";
-            isFirstCondition = false;
-        }else {
-            jpql += " and";
-        }
-        jpql += " o.status = :status";
-
-        //회원 이름 검색
-        if(StringUtils.hasText(orderSearch.getMemberName())) {
-            if( isFirstCondition ) {
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
                 jpql += " where";
-            }else {
+                isFirstCondition = false;
+            } else {
                 jpql += " and";
             }
-            jpql += "m.name like :name";
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
         }
         TypedQuery<Order> query = em.createQuery(jpql, Order.class)
                 .setMaxResults(1000); //최대 1000건
@@ -80,35 +68,65 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+    /**
+     * JPA Criteria
+     */
     public List<Order> findAllByCriteria(OrderSearch orderSearch) {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Order> cq = cb.createQuery(Order.class);
         Root<Order> o = cq.from(Order.class);
-        Join<Object, Object> m = o.join("member", JoinType.INNER);
-
+        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
         List<Predicate> criteria = new ArrayList<>();
-
         //주문 상태 검색
-        if( orderSearch.getOrderStatus() != null ){
-            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"),
+                    orderSearch.getOrderStatus());
             criteria.add(status);
         }
-
         //회원 이름 검색
-        if( StringUtils.hasText(orderSearch.getMemberName()) ) {
-            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+                    cb.like(m.<String>get("name"), "%" +
+                            orderSearch.getMemberName() + "%");
             criteria.add(name);
         }
-
         cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
-        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대1000건
         return query.getResultList();
     }
 
-    // 패치 조인을 이용
     public List<Order> findAllWithMemberDelivery() {
-        return em.createQuery("select o from Order o " +
-                " join fetch o.member m" +
-                " join fetch m.delivery d", Order.class).getResultList();
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .getResultList();
     }
+
+    // distinct의 역할 : db에서도 query문에 distinct가 날라가긴 함
+    // db에서 distinct는 컬럼의 값이 완전히 일치해야 distinct 처리가 됨
+    // 하지만 JPA는 동일한 식별자를 갖고 있는 데이터를 중복 제거까지 해주는 역할을 함
+    // JPA distinct : run db query + remove the same identifier data
+    // 중요! 1:N관계를 fetch join 하는 순간 페이징 처리는 불가능해진다
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                        "select distinct o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d" +
+                                " join fetch o.orderItems oi" +
+                                " join fetch oi.item i", Order.class)
+                 .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
 }
+
