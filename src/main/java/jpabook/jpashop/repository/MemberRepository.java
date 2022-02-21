af0276429661b57b712577847a65fcf8c405b17b
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-21 22:44:30 +0900

    interface

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,48 +1,12 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor //final이 붙거나 @NotNull이 붙은 필드의 생성자를 자동 생성
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @Autowired
-    private final EntityManager em;
-
-    // 저장
-    public void save(Member member) {
-        em.persist(member);
-    }
-
-    // 한명의 멤버 조회
-    // public <T> T find(Class<T> entityClass, ~) => entity 클래스로 반환
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    //TODO Optional로 변경예정
-    // 멤버 리스트 조회
-    public List<Member> findAll() {
-        List<Member> result = em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-
-        return result;
-    }
-
-    //TODO Optional로 변경예정
-    // 이름으로 조회
-    public List<Member> findByName(String name) {
-        List<Member> result = em.createQuery("select m from Member m where name = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
-
-        return result;
-    }
-    
+    // select m from Member m where mn.nae = ?
+    List<Member> findByName(String name);
 }
